886e6696003d5d3c2e3792e2ccc6f50fbeec9a7f
jcbbb <avaz28082000@gmail.com>
2021-07-11 00:13:20 +0500

    fit trailing slash

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useRoutes } from "../hooks/useRoutes";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
 import NotFound from "../components/404/404";
 
 function Routes() {
+  const { pathname } = useLocation();
   const routes = useRoutes();
   return (
     <Switch>
+      <Redirect from="/:url*(/+)" to={pathname.slice(0, -1)} />
       {routes.map((route, i) => (
         <Route key={i} component={route.component} path={route.path} exact={route.exact} />
       ))}
