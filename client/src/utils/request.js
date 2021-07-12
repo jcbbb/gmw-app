f6f9e200fa46672eb30bcf95676b7a9f405c1085
jcbbb <avaz28082000@gmail.com>
2021-07-12 23:47:23 +0500

    set correct headers for json

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -1,9 +1,19 @@
 export async function request(endpoint, { body, stringify = true, ...customConfig } = {}) {
+  const headers = stringify
+    ? {
+        "Content-Type": "application/json",
+      }
+    : {};
+
   const config = {
     method: body ? "POST" : "GET",
     body: body && stringify ? JSON.stringify(body) : body || undefined,
     credentials: "include",
     ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
   };
 
   return window.fetch(endpoint, config).then(async (response) => {
